ec8c49642dfd44f19496747359c52ffc165989d9
noah- <noah-@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2017-06-24 05:41:59 +0000

    Branch of bob-18 with Tye77920's 1.14d ptrs

--- JSApi/Socket.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-JSAPI_FUNC(Socket);
-
-extern JSClass socket_class;
-extern JSClassSpec socket_spec;
-extern JSFunctionSpec socket_methods[];
-extern JSPropertySpec socket_props[];
-extern JSFunctionSpec socket_static_methods[];
